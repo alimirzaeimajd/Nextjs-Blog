236c1d4dae50dbd99b63a606d3abf0cc66ca781c
alimirzaeimajd <pe4090731@gmail.com>
2023-07-01 04:39:27 -0700

    code snippet, add another post

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,6 +1,7 @@
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
-
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { coldarkDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 
@@ -20,7 +21,7 @@ function PostContent(props) {
     //     />
     //   );
     // },
-    p(paragraph) {
+    paragraph(paragraph) {
       const { node } = paragraph;
 
       if (node.children[0].tagName === "img") {
@@ -39,6 +40,17 @@ function PostContent(props) {
       }
 
       return <p>{paragraph.children}</p>;
+    },
+    code(code) {
+      const { className, children } = code;
+      const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={coldarkDark}
+          language={language}
+          children={children}
+        />
+      );
     },
   };
 
