2a512b460017cf4cd3153955953ad14803dab3e5
alimirzaeimajd <pe4090731@gmail.com>
2023-07-02 14:42:55 -0700

    just add mongoDB, not working

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,6 +1,6 @@
-import React from "react";
+import { MongoClient } from "mongodb";
 
-const handler = (req, res) => {
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
     if (
@@ -21,12 +21,34 @@ const handler = (req, res) => {
       message,
     };
 
-    console.log(newMessage);
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://alimirzaeimajd:jv3P64kNryaS5qc@cluster0.yeiqmbc.mongodb.net/my-site?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database." });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed!" });
+      return;
+    }
+
+    client.close();
 
     res
       .status(201)
       .json({ message: "Successfully stored message!", message: newMessage });
   }
-};
+}
 
 export default handler;
